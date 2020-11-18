77528085b589c339dc5eef7d988fa54ad440e413
aldi wirawan <aldiwira.aw@gmail.com>
2020-11-18 12:44:51 +0700

    Change framework

---
+++ application/views/Users/Home.php
@@ -0,0 +1,20 @@
+<div class="wrapper">
+    <div class="title-wrapper">
+        <div clas="wrapper-title-web">
+            <h1>Exoindo</h1>
+            <h3>Cleaning Service</h3>
+        </div>
+        <div class="btn-action-wrapper">
+            <a href="http://">
+                <button type="button" class="btn btn-primary">Login</button>
+            </a>
+            <a href="http://">
+                <button type="button" class="btn btn-secondary">Register</button>
+            </a>
+            
+        </div>
+    </div>
+    <div class="assets-wrapper">
+        
+    </div>
+</div>
